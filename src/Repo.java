7a5f7f64987f3630b59f7de5df23a6dfeb1f97d6
Dragomirescu Ionut <ionutdragomirescu6@gmail.com>
2023-09-08 19:49:23 +0300

    Updated Test class with a comment

--- src/Repo.java
+++
@@ -1,5 +0,0 @@
-public class Repo {
-   public void newRepo(){
-       System.out.println("This is also a new repo, but part of a class and much much cooler!");
-   }
-}
---
+++ src/session2/Repo.java
@@ -0,0 +1,7 @@
+package session2;
+
+public class Repo {
+    public void newRepo(){
+        System.out.println("Hello!");
+    }
+}
