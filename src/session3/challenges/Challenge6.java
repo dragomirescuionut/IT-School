fa6ba8ca552d90b5aab0c4f67897fcee1d568ed6
Dragomirescu Ionut <ionutdragomirescu6@gmail.com>
2023-09-11 16:51:14 +0300

    Session 3

---
+++ src/session3/challenges/Challenge6.java
@@ -0,0 +1,20 @@
+package session3.challenges;
+
+import java.util.Scanner;
+
+public class Challenge6 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter first number: ");
+        int number1 = scanner.nextInt();
+        System.out.println("Please enter second number: ");
+        int number2 = scanner.nextInt();
+        if (number1 > number2) {
+            System.out.println(number1 + " is greater than " + number2);
+        } else if (number1 < number2) {
+            System.out.println(number1 + " is less than " + number2);
+        } else {
+            System.out.println(number1 + " is equal to " + number2);
+        }
+    }
+}
