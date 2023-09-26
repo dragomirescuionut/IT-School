d48d28aac4547e1e4d57b7852ab86c01e6afc73e
Ionut D. <dragomirescuionut1@yahoo.com>
2023-09-26 17:26:56 +0300

    Added session 7 (first challenges + practice). Added first challenges for session 6.

---
+++ src/session7/practice/TimestampPractice.java
@@ -0,0 +1,17 @@
+package session7.practice;
+
+import java.sql.Time;
+import java.sql.Timestamp;
+
+public class TimestampPractice {
+    public static void main(String[] args) {
+        Timestamp currentTimestamp = new Timestamp(System.currentTimeMillis());
+        System.out.println("Current timestamp: " + currentTimestamp);
+
+        Timestamp timestamp1 = Timestamp.valueOf("2023-09-20 15:20:30.2343");
+        Timestamp timestamp2 = Timestamp.valueOf("2023-09-20 15:21:30.2343");
+
+        long differenceInSeconds = (timestamp2.getTime() - timestamp1.getTime()) / 1000;
+        System.out.println("Difference in seconds: " + differenceInSeconds);
+    }
+}
